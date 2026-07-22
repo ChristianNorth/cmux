1ef562ed62efc3ba6e51d54e5dc9251cd5887f43
Christian North <christiannorth@mercor.com>
2026-07-22 12:48:44 -0700

    Refresh pane overlay after window resize

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -3258,6 +3258,7 @@ struct ContentView: View {
             clampSidebarWidthIfNeeded(availableWidth: availableWidth)
             clampRightSidebarWidthIfNeeded(availableWidth: availableWidth)
             updateSidebarResizerBandState()
+            scheduleTmuxWorkspacePaneWindowOverlayGeometryRefresh(in: window)
         })
 
         view = AnyView(view.onChange(of: rightSidebarMaxWidthSetting) { _, _ in
